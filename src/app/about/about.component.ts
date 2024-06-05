fab96d5174aff716bcc7801bb00a0ea5bbff39df
HugoMelin <hugo.melin01@gmail.com>
2024-06-05 17:42:52 +0200

    Add content in about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AboutService } from '../services/about.service';
+import { TitreH1Component } from '../titres/titre-h1/titre-h1.component';
+import { TitreH2Component } from '../titres/titre-h2/titre-h2.component';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ TitreH1Component, TitreH2Component, NgFor, RouterLink ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit{
+  about:any = {}
+  constructor(private aboutService: AboutService) {}
 
+  ngOnInit(): void {
+      this.about = this.aboutService.about
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,5 @@ import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent, title: 'Bienvenue Au petit village'},
-    {path:"contact", component: AboutComponent, title: "Nous contacter - Au petit village"}
+    {path:"a-propos", component: AboutComponent, title: "Nous contacter - Au petit village"}
 ];
