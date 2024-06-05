26d02401a4366c0e99cc60e442e5b5b6ccaeb8da
HugoMelin <hugo.melin01@gmail.com>
2024-06-05 18:17:20 +0200

    Add content in contact page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent  } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent, title: 'Bienvenue Au petit village'},
-    {path:"a-propos", component: AboutComponent, title: "Nous contacter - Au petit village"}
+    {path:"a-propos", component: AboutComponent, title: "A propos de nous - Au petit village"},
+    {path: "contact", component: ContactComponent, title: "Nous contacter - Au petit village"}
 ];
