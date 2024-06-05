371c5b11bbb2d0d83e4b908a3ef45af5443d3beb
HugoMelin <hugo.melin01@gmail.com>
2024-06-05 17:14:34 +0200

    Add image in about service

--- src/app/services/about.service.ts
+++ src/app/services/about.service.ts
@@ -9,7 +9,11 @@ export class AboutService {
     paragraphes: [
       `Vous-êtes nostalgique des aventures rocambolesques d'Astérix et Obélix, ces irréductibles Gaulois qui ont bravé l'envahisseur romain ? Chez "Au petit village", nous partageons cette passion pour le célèbre village gaulois en confectionnant des figurines uniques à l'effigie de vos personnages préférés.`,
       `Que vous soyez amateur de la potion magique ou adepte des gauloiseries, nos créations artisanales sauront raviver vos souvenirs d'enfance. Laissez-vous transporter dans cet univers plein d'humour et de rebondissements, idéal pour les grands enfants de 30 à 55 ans !`
-    ]
+    ],
+    image: {
+      src: "assets/img/about-bg.jpg",
+      alt: "Bienvenue au Petit village."
+    }
   }
   constructor() { }
 }
