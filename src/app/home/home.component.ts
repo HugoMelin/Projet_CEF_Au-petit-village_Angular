8a22ce676d0c9798e2b26911b182e700c0cb0e8f
HugoMelin <hugo.melin01@gmail.com>
2024-06-05 09:22:39 +0200

    Add h1 component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutService } from '../services/about.service';
+import { TitreH1Component } from '../titres/titre-h1/titre-h1.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TitreH1Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
