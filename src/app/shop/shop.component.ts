3dc009b6e63dd70a9ac84c5a3e8bc38cac61a701
HugoMelin <hugo.melin01@gmail.com>
2024-06-07 09:09:31 +0200

    Add pipe for sorting et filter

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,15 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { ShopDataService } from '../services/shop-data.service';
 import { TitreH3Component } from '../titres/titre-h3/titre-h3.component';
+import { FormsModule } from '@angular/forms';
+import { FiltrePipe } from '../pipes/filtre.pipe';
+import { SortPipe } from '../pipes/sort.pipe';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [NgFor, TitreH3Component],
+  imports: [NgFor, TitreH3Component, FormsModule, FiltrePipe, SortPipe],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent implements OnInit{
+  searchTerm:string = ""
+  tri:string = ""
+
   productList:any = {}
   constructor(private shopDataService: ShopDataService) {}
 
