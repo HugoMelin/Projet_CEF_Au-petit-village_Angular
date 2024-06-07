1267b74de8e4d595d7975ce3c7b66b832774bab3
HugoMelin <hugo.melin01@gmail.com>
2024-06-07 23:13:02 +0200

    Add product page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent  } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './product/product.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent, title: 'Bienvenue Au petit village'},
     {path:"a-propos", component: AboutComponent, title: "A propos de nous - Au petit village"},
-    {path: "contact", component: ContactComponent, title: "Nous contacter - Au petit village"}
+    {path: "contact", component: ContactComponent, title: "Nous contacter - Au petit village"},
+    { path: 'product/:id', component: ProductComponent }
 ];
